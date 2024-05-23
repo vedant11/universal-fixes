13a31f74c076511021618d45375f0d5a2d7de5d9
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2024-05-23 22:15:07 +0530

    install chrome

---
+++ tools/install_chrome.sh
@@ -0,0 +1,3 @@
+curl -fSsL https://dl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor | sudo tee /usr/share/keyrings/google-chrome.gpg >> /dev/null
+sudo apt update
+sudo apt install google-chrome-stable
