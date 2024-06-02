015f2dddebdd241bc4a33b50c0da7f7236907d06
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2024-06-03 05:22:15 +0530

    Update wsl.sh for Windows Home Edition

--- OS/wsl.sh
+++ OS/wsl.sh
@@ -13,6 +13,15 @@ wsl.exe config --default-user <username>
 # force reclaim storage
 wsl --manage <distro> --set-sparse false
 optimize-vhd -Path pathto\ext4.vhdx -Mode full
+# for windows home edition: 
+: '
+diskpart
+select vdisk file="C:\WSL-Distros\…\ext4.vhdx"
+attach vdisk readonly
+compact vdisk
+detach vdisk
+exit
+'
 wsl --manage <distro> --set-sparse true # to turn on auto-reclaiming of storage
 
 # get path of external virtualized disk
