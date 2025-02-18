59d52f8fbbbb76253784d6ee9cdf1a963e526311
Vedant Panchal <pvipulkumar@me.iitr.ac.in>
2025-02-18 11:00:57 +0530

    add psql user creation

--- local_server/macos_psql.sh
+++ local_server/macos_psql.sh
@@ -8,3 +8,7 @@ source ~/.zshrc
 # postgresql@14 is the latest version
 psql --version
 psql postgres
+
+# creating a user inline from zsh for demo, should be done in the psql shell itself
+psql postgres -c "CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres';"
+psql postgres -c "ALTER ROLE postgres CREATEDB;"
